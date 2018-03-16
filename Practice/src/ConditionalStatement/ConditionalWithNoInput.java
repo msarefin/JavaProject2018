b3d165956cdb8ff0bfce18af7b93d578ddfceced
Sunny <mohammed.arefin@gmail.com>
2018-03-16 10:48:46 -0400

    added conditional statement without input!

---
+++ Practice/src/ConditionalStatement/ConditionalWithNoInput.java
@@ -0,0 +1,16 @@
+package ConditionalStatement;
+
+
+public class ConditionalWithNoInput {
+
+	public static void main(String[] args) {
+		int age = 25; 
+		int money = 500;
+		
+		if(age >=18) {
+			if(money >=500) {
+				System.out.println("Welcome!!!");
+			}
+		}
+	}
+}
