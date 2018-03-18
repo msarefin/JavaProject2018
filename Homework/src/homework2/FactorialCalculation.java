1c10efecf57c2506e9853d52153635628aac3a98
Sunny <mohammed.arefin@gmail.com>
2018-03-17 21:28:15 -0400

    updated

---
+++ Homework/src/homework2/FactorialCalculation.java
@@ -0,0 +1,12 @@
+package homework2;
+
+public class FactorialCalculation {
+
+	public static void main(String[]args) {
+		
+		for(int i = 1; i<=100;i++) {
+			
+		}
+		
+	}
+}
---
+++ Practice/src/practicing/practice.java
@@ -0,0 +1,15 @@
+package practicing;
+
+public class practice {
+	
+	public static void main(String[] args) {
+		
+		int i = 10;
+		
+		do {
+			System.out.println(i);
+			i--;
+		}
+		while (i>=0);
+	}
+}
