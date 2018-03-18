e80c9a7fb988313c9b4ac980897a27a0ff3fd73e
Sunny <mohammed.arefin@gmail.com>
2018-03-18 00:44:07 -0400

    updated

---
+++ Homework/src/homework2/JavaHomeworkTest.java
@@ -0,0 +1,92 @@
+package homework2;
+
+public class JavaHomeworkTest {
+
+	public static void main(String[]args) {
+		
+//		String[] a = new String[3];
+//		
+//		a[0] = "peoplentech";
+//		a[1] ="techhhhh";
+//		a[2] = "javalearningggg";
+//		
+//		System.out.println(a[0]+" "+a[0].length());
+//		System.out.println(a[1]+" "+a[1].length());
+//		System.out.println(a[2]+" "+a[2].length());
+//
+//		int[] n = new int[3];
+//		
+//		n[0]= a[0].length();
+//		n[1]= a[1].length();
+//		n[2]= a[2].length();
+//		
+//		for(int i =0; i <=2;i++) {
+//			if(n[i]>10) {
+//				System.out.println(a[i]+":::: it is a large String!");
+//			}
+//		}
+		
+//		int a = 10; 
+//		int b = 25; 
+//		int c = 35; 
+//		int d = -10; 
+//		String m = "Maximum is ";
+//		String n = "Minimum is ";
+//		
+//		//-------------Maximum------------------
+//		
+//		if(a>b && a>c && a>d) {
+//			System.out.println(m+a);
+//		}
+//		else if(b>a && b>c && b>d) {
+//			System.out.println(m+b);
+//		}
+//		else if(c>a && c>b &&c>d) {
+//			System.out.println(m+c);
+//		}
+//		else {
+//			System.out.println(m+d);
+//		}
+//		
+//		//-------------Minimum------------------
+//		
+//		if(a<b && a<c && a<d) {
+//			System.out.println(n+a);
+//		}
+//		else if(b<a && b<c && b<d) {
+//			System.out.println(n+b);
+//		}
+//		else if(c<a && c<b && c<d) {
+//			System.out.println(n+c);
+//		}
+//		else {
+//			System.out.println(n+d);
+//		}
+//		
+//		int sum = 0;
+//		
+//		for(int i=10; i<=99; i++) {
+//			
+//			sum=sum+i;
+//			//System.out.println(i + " "+ sum);
+//			
+//		}
+//		
+//		System.out.println("The sum of numbers between 10 and 99 is " + sum);
+//		
+		
+		for (int i = 1; i <= 10; i++) 
+        {
+            int num = i;
+             
+            for (int j = 1; j <= i; j++) 
+            {
+                System.out.print(num+" ");
+                 
+                num = num+10s-j;
+            }
+             
+            System.out.println();
+        }
+	}
+}
--- JavaClass2018/src/Day3/Looping.java
+++ JavaClass2018/src/Day3/Looping.java
@@ -2,4 +2,19 @@ package Day3;
 
 public class Looping {
 
+public static void main(String[]args) {
+				
+		int sum = 0;
+		
+		for(int i=10; i<=99; i++) {
+			
+			sum=sum+i;
+			//System.out.println(i + " "+ sum);
+			
+		}
+		
+		System.out.println("The sum of numbers between 10 and 99 is " + sum);
+		
+	}
 }
+
