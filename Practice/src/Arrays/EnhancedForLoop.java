7adf2f1bd54e5b61a11721cfa24106db0d740c91
Sunny <mohammed.arefin@gmail.com>
2018-03-22 08:46:36 -0400

    update

--- Practice/src/Arrays/EnhancedForLoop.java
+++ Practice/src/Arrays/EnhancedForLoop.java
@@ -6,9 +6,14 @@ public class EnhancedForLoop {
 		// Enhanced for loop
 		
 		int[] a = {1,3,5,7};
-		
+		String[] b = {"James", "John", "alex","Jonathan"};
 		for(int t: a) {
 			System.out.println("The value of Array is "+t);
+			
+		}
+		
+		for(String i: b) {
+			System.out.println("The Vale of b is "+b);
 		}
 	}
 }
---
+++ TestProject/src/testing/StarArray.java
@@ -0,0 +1,12 @@
+package testing;
+
+public class StarArray {
+	
+	public  static void main(Strings [] args) {
+		
+		char [] star = new char;
+		
+		
+	}
+
+}
