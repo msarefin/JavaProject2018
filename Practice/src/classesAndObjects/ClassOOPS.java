1009242fc17cad3719efd7cae4d57735cb707420
Sunny <mohammed.arefin@gmail.com>
2018-03-26 11:52:17 -0400

    Default Public Private Protected

--- Practice/src/classesAndObjects/ClassOOPS.java
+++ Practice/src/classesAndObjects/ClassOOPS.java
@@ -14,6 +14,27 @@ public class ClassOOPS {
 		OOPSMethod("Danile");
 		OOPSMethod("Stanley");
 		
+		Animal dog = new Animal();
+		
+		
+		System.out.print(dog.dogs);
+		dog.bark();
+		
+		
+		Vehicle v1 = new Vehicle();
+		Vehicle v2 = new Vehicle(); 
+		v1.color = "red";
+		v1.horn();
+		v1.fuelCapacity = 12.5;
+		v1.maxSpeed = 180;
+		v1.wheels =4; 
+		
+		System.out.println(v1.color);
+		System.out.println(v1.fuelCapacity);
+		System.out.println(v1.maxSpeed);
+		System.out.println(v1.wheels);
+		
+		v2.horn();
 		
 	}
 	
