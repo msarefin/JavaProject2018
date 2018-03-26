01c9e064bd646edb6aab8bb910640ad6d02574f1
Sunny <mohammed.arefin@gmail.com>
2018-03-26 01:35:01 -0400

    Learning OOPS

---
+++ Practice/src/classesAndObjects/ClassOOPS.java
@@ -0,0 +1,20 @@
+package classesAndObjects;
+
+public class ClassOOPS {
+
+	static void OOPSMethod(String name) {
+		
+		
+		
+		System.out.println("Hello "+ name);
+	}
+	
+	public static void main (String[] args) {
+		OOPSMethod("Sunny");
+		OOPSMethod("Danile");
+		OOPSMethod("Stanley");
+		
+		
+	}
+	
+}
---
+++ Practice/src/classesAndObjects/OOPS.java
@@ -0,0 +1,26 @@
+package classesAndObjects;
+
+public class OOPS {
+	
+	static void SayHello() {
+		
+		String OOP = "OOP is a organized around objects rather than action and data rather than logic";
+		String OOPS = "In Object orriented programming each each object has 3 dimensions: Identity, Attribute, Behabiour";
+		String Class = "A clas is a blueprint,description or defintion of objects";
+		String Methods = "A method is a collection of statement that are grouped to perform an operaton";
+		System.out.println("Hello World!!"); 
+		System.out.println(OOP);
+		System.out.println(OOPS);
+		System.out.println(Class);
+		System.out.println(Methods);
+		
+		
+	}
+	
+	
+	public static void main (String[] args) {
+		
+		SayHello();
+		
+	}
+}
