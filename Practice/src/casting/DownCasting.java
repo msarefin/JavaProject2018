f9ea19c42b064574579dd6f8bfca225a84e1087f
Sunny <mohammed.arefin@gmail.com>
2018-04-02 19:50:08 -0400

    update

--- Practice/src/casting/DownCasting.java
+++ Practice/src/casting/DownCasting.java
@@ -2,13 +2,13 @@ package casting;
 
 class A{
 	void mks() {
-		System.out.println("Noise");
+		System.out.println("This from class A");
 	}
 }
 
 class B extends A{
 	void mks() {
-		System.out.println("Meow");
+		System.out.println("This is from clas B ");
 	}
 	
 	void lgs(int l) {
@@ -23,20 +23,23 @@ public class DownCasting {
 		a.mks();
 		
 		B b = new B();
-		b.mks();
-		b.lgs(4);
+		b.mks(); // Meow
+		b.lgs(4); //4
 		
-		
+		System.out.println();
 //		Upcasting
 		
-		A a1 = b; // Same as A a1 = new B();
-		a1.mks();
+		A a1 = b; 
 		
+		// Same as A a1 = new B();
+		a1.mks(); // This is from class B 
+		
+		System.out.println();
 //		Downcasting
 		
 		B b1 = (B)a1;
-		b1.mks();
-		b1.lgs(3);
+		b1.mks(); // This is from class B 
+		b1.lgs(3); // 3
 		
 		
 	}
