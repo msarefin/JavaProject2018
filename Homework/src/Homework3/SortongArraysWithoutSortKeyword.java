b80375c8cb06969766e257d37a1d0a5d1ad024e0
Sunny <mohammed.arefin@gmail.com>
2018-04-08 22:00:26 -0400

    update

--- Homework/src/Homework3/SortongArraysWithoutSortKeyword.java
+++ Homework/src/Homework3/SortongArraysWithoutSortKeyword.java
@@ -2,20 +2,29 @@ package Homework3;
 
 public class SortongArraysWithoutSortKeyword {
 
-	public static void main (String[] args) {
+	public static void main(String[] args) {
+
+//		sorting arrays without the sort function
 		
-		int [] a = {3,7,9,10,6};
+		int[] a = { 3, 7, 9, 10, 6 };
 		int x = a[0];
+
+		for (int i : a) {
+			System.out.print(i+" ");
+			
+		}
 		
-		for(int i = 0;i<a.length;i++) {
-			
-			
-			if(a[i]>x) {
-				x = a[i];
+		System.out.println();
+		for(int i = 0; i<a.length;i++) {
+			for(int j = 1;j<a.length; j++) {
+				if(a[i]>a[j]) {
+					System.out.print(a[i]+"| ");
+					System.out.println();
+					System.out.print(a[j]+" *");
+				}
 				
-				System.out.println(a[i]);
 			}
 		}
 	}
-	
+
 }
