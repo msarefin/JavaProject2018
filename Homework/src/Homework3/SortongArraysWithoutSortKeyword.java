1803d6bb05ecf2b79d23ac34a29bf148e068a2bd
Sunny <mohammed.arefin@gmail.com>
2018-04-09 10:11:19 -0400

    update

--- Homework/src/Homework3/SortongArraysWithoutSortKeyword.java
+++ Homework/src/Homework3/SortongArraysWithoutSortKeyword.java
@@ -14,17 +14,7 @@ public class SortongArraysWithoutSortKeyword {
 			
 		}
 		
-		System.out.println();
-		for(int i = 0; i<a.length;i++) {
-			for(int j = 1;j<a.length; j++) {
-				if(a[i]>a[j]) {
-					System.out.print(a[i]+"| ");
-					System.out.println();
-					System.out.print(a[j]+" *");
-				}
-				
-			}
-		}
+		
 	}
 
 }
