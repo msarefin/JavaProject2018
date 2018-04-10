e854e1cdf0be9f7d8de7018989f5341c43dfe8f1
Sunny <mohammed.arefin@gmail.com>
2018-04-10 12:28:24 -0400

    Working on homework 4

---
+++ Homework/src/Homework4/filterDuplicateArrays.java
@@ -0,0 +1,8 @@
+package Homework4;
+
+public class filterDuplicateArrays {
+
+	public static void main(String[] args) {
+		int [] arr= {2,3,5,6,7,2,5,4,};
+	}
+}
---
+++ Practice/src/Note/C.java
@@ -0,0 +1,5 @@
+package Note;
+
+public class C extends A implements B{
+
+}
