b64284ecf793380326f11cbb1faa1ecb1bca54ab
Sunny <mohammed.arefin@gmail.com>
2018-04-13 01:20:52 -0400

    update

--- AceWinSelenium/src/selenium/sel.java
+++ AceWinSelenium/src/selenium/sel.java
@@ -1,11 +1,18 @@
 package selenium;
 
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+
 public class sel {
 
 	public static void main(String[] args) {
 		
-		System.setProperty(", value)
+		System.setProperty("webdriver.gecko.driver","C:\\Users\\Aney\\Documents\\JavaProject2018\\AceWinSelenium\\geckodriver.exe");
 
+		WebDriver driver = new FirefoxDriver();
+		
+		driver.get("http://www.yahoo.com");
+		driver.close();
 	}
 
 }
