8bd0dd7b94f010250b4462b842063edcde9ac531
Sunny <mohammed.arefin@gmail.com>
2018-04-13 01:56:02 -0400

    Selenium Chrome Driver!

---
+++ AceWinSelenium/src/selenium/ChromeSelenium.java
@@ -0,0 +1,15 @@
+package selenium;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class ChromeSelenium {
+
+	public static void main (String[] args)	{
+		System.setProperty("webdriver.Chrome.driver", "C:\\Users\\Aney\\Documents\\JavaProject2018\\AceWinSelenium\\chromedriver.exe");
+	
+		WebDriver driver = new ChromeDriver();
+		driver.get("http://www.excite.com");
+		driver.close();
+	}
+}
