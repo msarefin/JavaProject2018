5d4fca2bf447078b82c7e10c8ec07de32245bb77
Sunny <mohammed.arefin@gmail.com>
2018-04-14 01:27:45 -0400

    updated

--- AceWinSelenium/src/selenium/ChromeSelenium.java
+++ AceWinSelenium/src/selenium/ChromeSelenium.java
@@ -5,11 +5,23 @@ import org.openqa.selenium.chrome.ChromeDriver;
 
 public class ChromeSelenium {
 
-	public static void main (String[] args)	{
-		System.setProperty("webdriver.Chrome.driver", "C:\\Users\\Aney\\Documents\\JavaProject2018\\AceWinSelenium\\chromedriver.exe");
-	
+	public static void main(String[] args) {
+		System.setProperty("webdriver.Chrome.driver",
+				"C:\\Users\\Aney\\Documents\\JavaProject2018\\AceWinSelenium\\chromedriver.exe");
+
 		WebDriver driver = new ChromeDriver();
+
 		driver.get("http://www.excite.com");
+
+		String PageTitle = driver.getTitle();
+		System.out.println(PageTitle);
+
+		String url = driver.getCurrentUrl();
+		System.out.println(url);
+
+		String PageSource = driver.getPageSource();
+		System.out.println(PageSource);
+
 		driver.close();
 	}
 }
---
+++ Practice/src/oopInterface/Kitten.java
@@ -0,0 +1,12 @@
+package oopInterface;
+
+public class Kitten {
+
+	public static void main(String[] args) {
+		
+		Animal a = new Dog();
+		
+		a.eat();
+		
+	}
+}
