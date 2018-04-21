34c89164ca2eb3d32e8b0e5e7dfa429d9c9007ba
Sunny <mohammed.arefin@gmail.com>
2018-04-21 19:23:19 -0400

    update

--- AceWinSelenium/src/selenium/FrameClass.java
+++ AceWinSelenium/src/selenium/FrameClass.java
@@ -33,20 +33,70 @@ public class FrameClass {
 		driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
 		
 		
-		driver.switchTo().frame("IF1"); // Switching for frame by name
+		
+		driver.switchTo().frame("IF1"); // Switching to frame by name
 		driver.findElement(By.xpath(".//*[@id='content']/div[1]/div/div/div/div[2]/div/form/fieldset/div[8]/input")).sendKeys("Alexander");
 		driver.findElement(By.xpath(".//*[@id='content']/div[1]/div/div/div/div[2]/div/form/fieldset/div[11]/input")).sendKeys("Hamilton");
 //		driver.findElement(By.linkText("Link Test")).click();
 		
+		url = driver.getCurrentUrl(); 
+		PageTitle = driver.getTitle();
+		
+		System.out.println(PageTitle);
 		driver.switchTo().defaultContent();
 		
-		driver.switchTo().frame(1);
+		url = driver.getCurrentUrl(); 
+		PageTitle = driver.getTitle();
+		System.out.println(PageTitle);
+		
+		/*
+		 * <iframe id="IF2" width="400" align="left" height="600" src="http://demoqa.com" name="iframe2"/>
+		 */
+		
+		driver.switchTo().frame(1); // Switching to frame by using index. The index starts from 0 and so the index 1 is the second frame in the list 
+		
+		url = driver.getCurrentUrl(); 
+		PageTitle = driver.getTitle();
+		System.out.println(PageTitle);
 		
 		driver.findElement(By.xpath(".//*[@id='ui-id-5']")).click();
 		
 		System.out.println(driver.findElement(By.xpath(".//*[@id='tabs-1']/p[2]")).getText());
 		
+		driver.switchTo().defaultContent();
+		
+driver.switchTo().frame("IF2"); // Switching to frame by using index. The index starts from 0 and so the index 1 is the second frame in the list 
+		
+		url = driver.getCurrentUrl(); 
+		PageTitle = driver.getTitle();
+		System.out.println(PageTitle);
+		
+		driver.findElement(By.xpath(".//*[@id='ui-id-5']")).click();
+		
+		System.out.println(driver.findElement(By.xpath(".//*[@id='tabs-1']/p[2]")).getText());
+		
+		driver.switchTo().defaultContent();
+		
+driver.switchTo().frame("iframe2"); // Switching to frame by using index. The index starts from 0 and so the index 1 is the second frame in the list 
+		
+		url = driver.getCurrentUrl(); 
+		PageTitle = driver.getTitle();
+		System.out.println(PageTitle);
+		
+		driver.findElement(By.xpath(".//*[@id='ui-id-5']")).click();
+		
+		System.out.println(driver.findElement(By.xpath(".//*[@id='tabs-1']/p[2]")).getText());
+		
+		driver.switchTo().defaultContent();
+		url = driver.getCurrentUrl(); 
+		PageTitle = driver.getTitle();
+		System.out.println(PageTitle);
 		Thread.sleep(1000);
+		
+		System.out.println("Find how many frames exist in the page");
+		
+		int x = driver.findElements(By.tagName("iframe")).size();
+		System.out.println("Thee are "+x+" frames in  this page");
 		
 		driver.quit();
 	}
