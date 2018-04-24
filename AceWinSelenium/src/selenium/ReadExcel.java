dfe3347a35dfffdd2f073bc3dc98a7b393ba9408
Sunny <mohammed.arefin@gmail.com>
2018-04-24 17:51:08 -0400

    update

--- AceWinSelenium/src/selenium/ReadExcel.java
+++ AceWinSelenium/src/selenium/ReadExcel.java
@@ -1,15 +1,25 @@
 package selenium;
 
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+
+import org.apache.poi.xssf.usermodel.XSSFCell;
+import org.apache.poi.xssf.usermodel.XSSFRow;
+import org.apache.poi.xssf.usermodel.XSSFSheet;
+import org.apache.poi.xssf.usermodel.XSSFWorkbook;
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 
 public class ReadExcel {
 
-//	This is to read the excel form the file
-	
-	public static void main (String[] args) {
-		
+	// This is to read the excel form the file
+
+	public static void main(String[] args) throws IOException, InterruptedException {
+
 		String os = System.getProperty("os.name").toLowerCase();
 		String webaddress, url, PageTitle;
 		webaddress = "http://www.facebook.com";
@@ -27,9 +37,57 @@ public class ReadExcel {
 		driver.manage().deleteAllCookies();
 		driver.manage().window().maximize();
 		driver.get(webaddress);
-		
-		
-		
-		
+
+		// File f1 = new
+		// File("C:\\Users\\Aney\\Documents\\JavaProject2018\\AceWinSelenium\\ReadExcel.xlsx");
+		// FileInputStream fis1 = new FileInputStream(f1);
+		// XSSFWorkbook wk1 = new XSSFWorkbook(fis1);
+		// XSSFSheet sh1 = wk1.getSheetAt(0);
+		// XSSFRow rw1 = sh1.getRow(1);
+		// XSSFCell cell1 = rw1.getCell(0);
+		// String value1 = cell1.getStringCellValue();
+		// System.out.println(value1);
+		//
+		// -------------------------------------------------------
+
+		// File fl = new File((System.getProperty("user.dir") + "\\ReadExcel.xlsx"));
+		// FileInputStream fis = new FileInputStream(fl);
+		// XSSFWorkbook wk = new XSSFWorkbook(fis);
+		// XSSFSheet sh = wk.getSheetAt(0);
+		// XSSFRow rw = sh.getRow(1);
+		// XSSFCell cell = rw.getCell(0);
+		// String value = cell.getStringCellValue();
+		// System.out.println(value);
+
+		/*
+		 * if you use xlsx use XSSF if you use xls use HSSF
+		 */
+
+		ReadExcel rxl = new ReadExcel();
+
+		driver.findElement(By.xpath(".//*[@id='email']")).sendKeys(rxl.readexcels(driver, 1, 2));
+
+		Thread.sleep(1000);
+
+		driver.quit();
+
+	}
+
+	public String readexcels(WebDriver driver, int col, int row) throws IOException {
+
+		File fl = new File((System.getProperty("user.dir") + "\\ReadExcel.xlsx"));
+		FileInputStream fis = new FileInputStream(fl);
+		XSSFWorkbook wk = new XSSFWorkbook(fis);
+		XSSFSheet sh = wk.getSheetAt(0);
+		XSSFRow rw = sh.getRow(row - 1);
+		XSSFCell cell = rw.getCell(col - 1);
+		String value = cell.getStringCellValue();
+		return value;
+		// System.out.println(value);
+
+		/*
+		 * if you use xlsx use XSSF if you use xls use HSSF
+		 */
+
 	}
 }
