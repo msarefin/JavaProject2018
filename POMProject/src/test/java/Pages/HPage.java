6aac1d8b41f232486dfd14ce79bb4f228e9744be
Sunny <mohammed.arefin@gmail.com>
2018-04-24 20:32:33 -0400

    downloaded the project

---
+++ POMProject/src/test/java/Pages/HPage.java
@@ -0,0 +1,14 @@
+package Pages;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+
+public class HPage {
+
+	public static Driver driver;
+	
+	public static void printTitle() {
+		
+	}
+}
