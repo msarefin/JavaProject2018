a5b81494e8253aa3f6d316b5629884b242971196
Sunny <mohammed.arefin@gmail.com>
2018-04-28 05:50:23 -0400

    update

--- MacSelenium/src/selenium/sel.java
+++ MacSelenium/src/selenium/sel.java
@@ -1,5 +1,24 @@
 package selenium;
 
+import java.util.concurrent.TimeUnit;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+
 public class sel {
 
+	public static void main(String [] args) {
+		
+		System.getProperty("webdriver.gecko.driver", "/Users/sunny/Documents/JavaProject2018/MacSelenium/geckodriver");
+		
+		WebDriver driver = new FirefoxDriver();
+		driver.get("http://www.rediff.com/");
+		driver.manage().deleteAllCookies();
+		driver.manage().window().maximize();
+		driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+		
+		driver.quit();
+	}
+	
 }
