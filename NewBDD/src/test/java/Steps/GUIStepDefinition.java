6cf9f2415956f74a23b9c2856c4ef977af2aec90
Sunny <mohammed.arefin@gmail.com>
2018-05-07 20:14:52 -0400

    update

---
+++ NewBDD/src/test/java/Steps/GUIStepDefinition.java
@@ -0,0 +1,56 @@
+package Steps;
+
+import java.util.concurrent.TimeUnit;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+import cucumber.api.java.After;
+import cucumber.api.java.Before;
+
+public class GUIStepDefinition {
+
+	WebDriver driver;
+
+	@Before
+	public void initialize() {
+		System.out.println("this is before class");
+		String os, address;
+
+		address = "https://www.facebook.com/";
+
+		os = System.getProperty("os.name").toLowerCase();
+
+		if (os.contains("mac")) {
+			System.out.println("Project Running on MAC");
+			System.setProperty("webdriver.chrome.driver", System.getProperty("user.dir") + "/Drivers/Mac/chromedriver");
+		} else if (os.contains("windows")) {
+			System.out.println("Project running on Windows");
+			System.setProperty("webdriver.chrome.driver",
+					System.getProperty("user.dir") + "\\Drivers\\Windows\\chromedriver.exe");
+		} else {
+			System.out.println("Project Running on Linux");
+		}
+
+		// Webdriver is interface
+		driver = new ChromeDriver();
+		driver.manage().window().maximize();
+
+		driver.get(address);
+		driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+
+	}
+
+//	----------------------------------------------------------------------------
+
+	
+	
+//	----------------------------------------------------------------------------
+	
+	@After
+	public void teardown() {
+		driver.manage().deleteAllCookies();
+		driver.quit();
+	}
+	
+}
