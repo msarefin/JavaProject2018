6df159d815022b59b8aa6bd899a2bdaf95685237
Sunny <mohammed.arefin@gmail.com>
2018-05-21 11:28:47 -0400

    fialed cross browser attempt!

--- mTestNG/src/test/java/corssBrowsers/VerifyTitle.java
+++ mTestNG/src/test/java/corssBrowsers/VerifyTitle.java
@@ -13,24 +13,30 @@ public class VerifyTitle {
 	
 	@Test(priority=1)
 	@Parameters({"browser"})
-	public void sam(String Browser) {
-		System.out.println("You are using " + Browser);
+	public void sam(String browser) {
+		System.out.println("Test1 /n");
+		System.out.println("You are using " + browser);
 	}
 	
 	@Test(priority = 2)
 	@Parameters({"browser"})
-	public void browsers(String browsername) {
+	public void browsers(String browser) {
 		
-		System.out.println(browsername);
+		System.out.println(browser);
 		
-		if(browsername.equalsIgnoreCase("chrome")) {
-			System.out.println("You have selected " + browsername);
+		if(browser.equalsIgnoreCase("chrome")) {
+			System.out.println("You have selected " + browser);
 			
 			System.setProperty("webdriver.chrome.driver",
 					"C:\\Users\\Aney\\Documents\\JavaProject2018\\mTestNG\\Drivers\\Windows\\chromedriver.exe");
 			
-//			driver = new ChromeDriver();
+			System.out.println("chrome driver location set! ");
 			
+			driver = new ChromeDriver();
+			
+			// Fialed here while running from xml 
+			
+			System.out.println("chrome launched");
 		}
 		else {
 			System.out.println("You have not selected chrome");
@@ -39,7 +45,7 @@ public class VerifyTitle {
 		driver.manage().window().maximize();
 		driver.get("www.yahoo.com");
 		driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
-		
+		System.out.println("Test1 /n");
 	}
 	
 	
