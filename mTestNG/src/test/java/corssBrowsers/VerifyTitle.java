1d61a898977248f6158fe800c58c1f721824621f
Sunny <mohammed.arefin@gmail.com>
2018-05-20 21:06:04 -0400

    update

---
+++ mTestNG/src/test/java/corssBrowsers/VerifyTitle.java
@@ -0,0 +1,36 @@
+package corssBrowsers;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.testng.annotations.Parameters;
+import org.testng.annotations.Test;
+
+public class VerifyTitle {
+
+	WebDriver driver;
+	
+	
+	@Test
+	@Parameters("webBrowser")
+	public void verifyTitle(String browsername) {
+		
+		if(browsername.equalsIgnoreCase("firefox")) {
+			
+			driver = new FirefoxDriver();
+		}
+		else if(browsername.equalsIgnoreCase("chromeBrowser")) {
+			
+			System.setProperty("webdriver.chrome.driver", "C:\\Users\\Aney\\Documents\\JavaProject2018\\mTestNG\\Drivers\\Windows\\chromedriver.exe");
+			
+			driver = new ChromeDriver();
+		}
+	
+		driver.manage().window().maximize();
+		driver.get("http://www.google.com");
+		driver.quit();
+		
+	}
+	
+	
+}
