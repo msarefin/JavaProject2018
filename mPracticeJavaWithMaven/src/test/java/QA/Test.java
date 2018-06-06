e3fd4f5c5a4d8f0d5100731c69bc51032ff26380
masrefin <mohammed.arefin@gmail.com>
2018-06-06 01:57:48 -0400

    update

--- mPracticeJavaWithMaven/src/test/java/QA/Test.java
+++ mPracticeJavaWithMaven/src/test/java/QA/Test.java
@@ -1,12 +1,16 @@
 package QA;
 
+import java.util.List;
 import java.util.concurrent.TimeUnit;
 
 import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
 import org.openqa.selenium.Point;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.interactions.Action;
+import org.openqa.selenium.interactions.Actions;
 
 public class Test {
 
@@ -59,8 +63,74 @@ public class Test {
 		System.out.println(title.length());
 		
 		System.out.println(driver.getPageSource().length());
+		driver.navigate().to("http://www.facebook.com");
+		System.out.println(driver.getTitle());
+		
+		boolean selection = driver.findElement(By.cssSelector("input[name='sex']")).isSelected();
+		
+		if(selection) {
+			System.out.println(driver.findElement(By.xpath("//label[contains(text(),'Female')]")).getText() + " is selected");
+		}else {
+			System.out.println(driver.findElement(By.xpath("//label[contains(text(),'Female')]")).getText() +" is not selected");
+		}
+		
+		driver.navigate().to("https://www.amazon.com/");
+		
+//		driver.findElement(By.cssSelector("input[id='twotabsearchtextbox']")).sendKeys("The art of the deal");
+//		
+//		driver.findElement(By.cssSelector("input[id='twotabsearchtextbox']")).clear();
+		
+		System.out.println(driver.getCurrentUrl());
+		
+		Actions ac = new Actions(driver);
+		
+		ac.moveToElement(driver.findElement(By.linkText("Departments"))).build().perform();
+		
+		
+		driver.navigate().to("https://www.united.com/ual/en/us/");
+		
+		driver.findElement(By.cssSelector("input#SearchTypeMain_oneWay")).click();
+		
+		boolean returns = driver.findElement(By.cssSelector("label[for='ReturnDate']")).isDisplayed();
+		
+		if(returns) {
+			System.out.println("return is enabled");
+		}
+		else {
+			System.out.println("return is disabled");
+		}
+		
+		driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+		System.out.println();
+//		Gets all the texts within the tag including the subtag
+		String text = driver.findElement(By.tagName("fieldset")).getText();
+		
+		System.out.println(text);
+		
+		text = driver.findElement(By.tagName("fieldset")).getTagName(); 
+		
+		Thread.sleep(1000);
+		
+		System.out.println();
+		
+		System.out.println(text);
+		
+text = driver.findElement(By.tagName("fieldset")).getAttribute("id"); 
+		
+		Thread.sleep(1000);
+		
+		System.out.println();
+		
+		System.out.println(text);
+		
+		System.out.println();
+		
+		
 		
 		Thread.sleep(5000);
+		
+//		driver.navigate().back();
+		
 		
 		driver.close();
 
