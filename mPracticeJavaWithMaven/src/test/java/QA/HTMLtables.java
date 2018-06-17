2cf7104f568d468b67f23fe68821e80cd639880a
Sunny <mohammed.arefin@gmail.com>
2018-06-16 22:15:08 -0400

    update

--- mPracticeJavaWithMaven/src/test/java/QA/HTMLtables.java
+++ mPracticeJavaWithMaven/src/test/java/QA/HTMLtables.java
@@ -1,10 +1,42 @@
 package QA;
 
+import java.util.concurrent.TimeUnit;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
 public class HTMLtables {
 
 	public static void main (String [] args ) {
 		
+		String os, address, url, title;
+
+		address = "https://www.swtestacademy.com/selenium-javascriptexecutor/";
+
+		os = System.getProperty("os.name").toLowerCase();
+
+		if (os.contains("mac")) {
+			System.out.println("Test Running from MAC");
+			System.setProperty("webdriver.chrome.driver", System.getProperty("user.dir") + "/Drivers/Mac/chromedriver");
+		} else if (os.contains("windows")) {
+			System.out.println("Test Running from Windows");
+			System.setProperty("webdriver.chrome.driver",
+					System.getProperty("user.dir") + "\\Drivers\\Windows\\chromedriver.exe");
+		} else {
+			System.out.println("Test Running from Linux");
+		}
+
+		WebDriver driver = new ChromeDriver();
+		driver.manage().deleteAllCookies();
+		driver.manage().window().maximize();
+
+		driver.get(address);
+		driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+		url = driver.getCurrentUrl();
+		title = driver.getTitle();
 		
+		
+		driver.quit();
 		
 	}
 	
--- mPracticeJavaWithMaven/src/test/java/QA/WorkingWithJavascriptExecutor.java
+++ mPracticeJavaWithMaven/src/test/java/QA/WorkingWithJavascriptExecutor.java
@@ -10,14 +10,14 @@ import org.openqa.selenium.chrome.ChromeDriver;
 
 public class WorkingWithJavascriptExecutor {
 
-	public static void main(String [] args) throws InterruptedException {
-		
-String os, address, url, title; 
-		
+	public static void main(String[] args) throws InterruptedException {
+
+		String os, address, url, title;
+
 		address = "https://www.swtestacademy.com/selenium-javascriptexecutor/";
-		
+
 		os = System.getProperty("os.name").toLowerCase();
-		
+
 		if (os.contains("mac")) {
 			System.out.println("Test Running from MAC");
 			System.setProperty("webdriver.chrome.driver", System.getProperty("user.dir") + "/Drivers/Mac/chromedriver");
@@ -28,30 +28,32 @@ String os, address, url, title;
 		} else {
 			System.out.println("Test Running from Linux");
 		}
-			
+
 		WebDriver driver = new ChromeDriver();
 		driver.manage().deleteAllCookies();
 		driver.manage().window().maximize();
-		
+
 		driver.get(address);
 		driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
-		url = driver.getCurrentUrl(); 
+		url = driver.getCurrentUrl();
 		title = driver.getTitle();
-		
-		JavascriptExecutor js = (JavascriptExecutor) driver; 
-		
-//		String str = js.executeScript("return document.getElementById('btn2').innerHTML").toString();
-//		System.out.println(str);
-//		js.executeScript("document.getElementById('btn2').innerHTML='I am Here'");
-		
-		//js.executeScript("arguments[0].style.border='3px solid red'", driver.findElement(By.xpath(".//*[@id='loginForm']/div/div/input")));
-		
-		js.executeScript("arguments[0].scrollIntoView(0);", driver.findElement(By.xpath(".//*[@id='post-1112']/div[2]/p[27]/a/img")));
-		
+
+		JavascriptExecutor js = (JavascriptExecutor) driver;
+
+		// String str = js.executeScript("return
+		// document.getElementById('btn2').innerHTML").toString();
+		// System.out.println(str);
+		// js.executeScript("document.getElementById('btn2').innerHTML='I am Here'");
+
+		// js.executeScript("arguments[0].style.border='3px solid red'",
+		// driver.findElement(By.xpath(".//*[@id='loginForm']/div/div/input")));
+
+		js.executeScript("arguments[0].scrollIntoView(0);",
+				driver.findElement(By.xpath(".//*[@id='post-1112']/div[2]/p[27]/a/img")));
+
 		Thread.sleep(10000);
-		
-		
+
 		driver.quit();
-		
+
 	}
 }
