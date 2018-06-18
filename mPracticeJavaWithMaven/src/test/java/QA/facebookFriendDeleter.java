bb7ea9003d300953eea76f036e040f67c97a5ad6
Sunny <mohammed.arefin@gmail.com>
2018-06-18 01:33:59 -0400

    updated

--- mPracticeJavaWithMaven/src/test/java/QA/facebookFriendDeleter.java
+++ mPracticeJavaWithMaven/src/test/java/QA/facebookFriendDeleter.java
@@ -1,12 +1,18 @@
 package QA;
 
+import java.util.List;
 import java.util.concurrent.TimeUnit;
 
 import org.openqa.selenium.By;
+import org.openqa.selenium.JavascriptExecutor;
+import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.interactions.Actions;
+import org.openqa.selenium.interactions.KeyDownAction;
+
+import gherkin.formatter.Argument;
 
 public class facebookFriendDeleter {
 
@@ -40,44 +46,68 @@ public class facebookFriendDeleter {
 		
 		driver.findElement(By.xpath(".//*[@id='email']")).sendKeys("");
 		driver.findElement(By.xpath(".//*[@id='pass']")).sendKeys("");
-		driver.findElement(By.xpath(".//*[@id='u_0_2']")).click();
+		driver.findElement(By.xpath("//label[@id='loginbutton']")).click();
 		driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
 		System.out.println("logged in");
+		Thread.sleep(2000);
 		
-		
-		System.out.println(driver.getTitle());
 		
 		Actions ac = new Actions(driver);
-		ac.moveToElement(driver.findElement(By.cssSelector("._2s25._606w"))).click().build().perform();
-		driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
-		System.out.println("clicked on profile");
-		
-		ac.moveToElement(driver.findElement(By.xpath("//span[@class='_1vp5']"))).click().build().perform();
-		
-		driver.findElement(By.xpath("//a[@data-tab-key='friends']")).click();
-		Thread.sleep(2000);
-		ac.moveToElement(driver.findElement(By.xpath("//a[@data-tab-key='friends']"))).click().build().perform();
-//		driver.findElement(By.xpath("//a[contains(text(),'Recently Added')]")).click();
-		
-		ac.moveToElement(driver.findElement(By.xpath("//span[contains(text(),'Recently Added')]"))).click().build().perform();
-		
+		JavascriptExecutor js = (JavascriptExecutor) driver;
+		ac.sendKeys(Keys.ESCAPE).build().perform();
+		ac.moveToElement(driver.findElement(By.xpath("(//div[@dir='ltr'])[1]"))).click().build().perform();
+//		js.executeScript("argument[0].click()", driver.findElement(By.xpath("//a[@title='Profile']")));
 		Thread.sleep(5000);
 		
+		System.out.println("on Porfile page");
 		
-		int size = driver.findElements(By.cssSelector("._5q6s._8o._8t.lfloat._ohe")).size();
+		ac.moveToElement(driver.findElement(By.xpath("//a[text()='Friends']"))).click().build().perform();
+		Thread.sleep(5000);
+		WebElement RFrend = driver.findElement(By.xpath("//span[text()='Recently Added']"));
+		Thread.sleep(2000);
+		try {
 		
-		for(int i =1; i<size;i++) {
+		RFrend.click();
+		}
+			catch(Exception e) {
+				js.executeScript("arguments[0].click()", RFrend);
+		}
+		js.executeScript("arguments[0].scrollIntoView()", RFrend);
+		Thread.sleep(2000);
+		String windowMain = driver.getWindowHandle(); 
+		System.out.println("Friends Clicked");
+		
+		List<WebElement> flist = driver.findElements(By.xpath("(//div[@aria-role='tabpanel'])/ul/li/div/div/div/div/div[2]/div"));
+		
+		int size = flist.size();
+		
+		System.out.println("There are "+size+" friends");
+		
+//		for(WebElement e: flist) {
+//			
+//			String link = e.getAttribute("href");
+//			System.out.println(link);
+//			
+//			ac.moveToElement(e).keyDown(Keys.CONTROL).click().keyUp(Keys.CONTROL).build().perform();
+//			
+//			Thread.sleep(1000);
+//		}
+		
+		for(int i= 0; i< size; i++) {
+			WebElement e = flist.get(i).findElement(By.tagName("a"));
 			
-			driver.findElements(By.cssSelector("._5q6s._8o._8t.lfloat._ohe")).get(i).click();
-			driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
-			driver.findElement(By.xpath(".//*[@id='u_ps_jsonp_5_0_d']")).click();
-			ac.moveToElement(driver.findElement(By.xpath(".//*[@id='u_ps_jsonp_5_0_0']/div/ul/li[7]/a/span/span"))).click();	
-			Thread.sleep(2000);
-			driver.navigate().back();
-			driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+			String a = e.getAttribute("href");
+			String name = e.getText().toString(); 
+			String t = e.getTagName(); 
+			System.out.println(name);
+		
 			
+			ac.moveToElement(e).keyDown(Keys.CONTROL).click().keyUp(Keys.CONTROL).build().perform();
+			
+//			Thread.sleep(2000);
 		}
 		
+//		System.out.println("links clicked");
 		driver.quit();
 
 	}
