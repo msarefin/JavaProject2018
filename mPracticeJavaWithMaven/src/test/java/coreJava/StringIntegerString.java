4fc93df83f6ec273f80bfdb900caa6f54de5939d
masrefin <mohammed.arefin@gmail.com>
2018-08-25 12:34:02 -0400

    update

---
+++ mPracticeJavaWithMaven/src/test/java/coreJava/StringIntegerString.java
@@ -0,0 +1,19 @@
+package coreJava;
+
+public class StringIntegerString {
+
+	public static void main(String[] args) {
+		
+		int i = 12; 
+		String msg = "32"; 
+		
+		
+		int msg1 = Integer.parseInt(msg);
+		String m = Integer.toString(i);
+		
+		System.out.println(msg1);
+		System.out.println(m);
+		
+		
+	}
+}
