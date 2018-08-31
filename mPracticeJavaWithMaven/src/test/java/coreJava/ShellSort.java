18faad5b0467313fc35b54156e7d72f6cea78387
masrefin <mohammed.arefin@gmail.com>
2018-08-30 22:26:04 -0400

    update

--- mPracticeJavaWithMaven/src/test/java/coreJava/ShellSort.java
+++ mPracticeJavaWithMaven/src/test/java/coreJava/ShellSort.java
@@ -3,59 +3,50 @@ package coreJava;
 import java.util.Random;
 
 //Java implementation of ShellSort
-class ShellSort
-{
-	
-	static void printArray(int arr[])
-	{
+class ShellSort {
+
+	static void printArray(int arr[]) {
 		int n = arr.length;
-		for (int i=0; i<n; ++i)
+		for (int i = 0; i < n; ++i)
 			System.out.print(arr[i] + " ");
 		System.out.println();
 	}
 
-	static int loopcount; 
-	
-	int sort(int arr[])
-	{
-	    //0,1,2,3,4,5,6
-	    //6,3,1,7,5,2,9
+	static int loopcount;
+
+	int sort(int arr[]) {
+
 		int n = arr.length;
 
-		        //3,1
-		for (int gap = n/2; gap > 0; gap /= 2)
-		{
-			
-			for (int i = gap; i < n; i += 1)
-			{
-				
+		for (int gap = n / 2; gap > 0; gap /= 2) {
+
+			for (int i = gap; i < n; i += 1) {
+
 				int temp = arr[i];
 
-				
 				int j;
 				for (j = i; j >= gap && arr[j - gap] > temp; j -= gap)
 					arr[j] = arr[j - gap];
 
-				
 				arr[j] = temp;
-				
+
 				loopcount++;
 			}
 		}
 		return 0;
 	}
 
-	
-	public static void main(String args[])
-	{
-		int arr[] = new int[25];
+	public static void main(String args[]) {
+		int arr[] = new int[10];
+
+		Random rand = new Random();
 		
-		Random rand = new Random(); 
 		
-		for(int i = 0; i<arr.length; i++) {
-			arr[i] = rand.nextInt(1000);
+
+		for (int i = 0; i < arr.length; i++) {
+			arr[i] = rand.nextInt(100);
 		}
-		
+
 		System.out.println("Array before sorting");
 		printArray(arr);
 
@@ -64,9 +55,7 @@ class ShellSort
 
 		System.out.println("Array after sorting");
 		printArray(arr);
-		
-		System.out.println("Loop Counter : \n"+loopcount);
+
+		System.out.println("Loop Counter : \n" + loopcount);
 	}
-} 
-
-
+}
